93c6ff8fea924159fd68ae2f24006ee7673c0242
Precious Worthy <preciousworthy@gmail.com>
2022-09-02 15:01:12 -0700

    Updates to app.delete and deleteBook()

--- API-books-project/client/front.js
+++ API-books-project/client/front.js
@@ -1,15 +1,31 @@
-async function showBooks() {
 
-    const URL = 'http://localhost:6066/api/books';
+async function deleteBook(isbn) {
+  const URL = 'http://localhost:6066/api/books';
+  const response = await fetch(URL + "/" + isbn, { method: 'DELETE'});
+    // Reloading the page
+    location.reload();
+}
+
+async function showBooks() {
+  const URL = 'http://localhost:6066/api/books';
     const response = await fetch(URL);
 
     const responseBooks = await response.json();
 
     for (let book of responseBooks) {
-        const card = `<div class="col-4">
-        <div class="card">
-        <div class="card-body">
-        <h5 class="class-title">${book.title}</h5>
+        const card = 
+        `<div class="col-4">
+          <div class="card">
+            <div class="card-body">
+            <hr>
+             <h5 class="card-title">${book.title}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">${book.isbn}<h6>
+
+              <div>Author: ${book.author}</div>
+              <div>Format: ${book.format}</div>
+              <br>
+
+              <button type="button" class="btn btn-danger" onClick="${deleteBook()} method="DELETE">Delete</button>
         </div>
         </div>
         </div>`
