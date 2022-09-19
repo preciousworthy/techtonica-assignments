76057bb1963347f46354f03b62177e8b15b11649
Precious Worthy <preciousworthy@gmail.com>
2022-09-19 15:39:12 -0700

    made components folder with footer component

---
+++ Eventonica/client/src/components/footer.jsx
@@ -0,0 +1,17 @@
+export default function Footer() {
+
+    return (
+        
+        <div>
+          Star Calendar favicon made by
+          <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+            Freepik
+          </a>
+          Find your own on
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            FlatIcon.com
+          </a>
+        </div>
+      
+    )
+}
